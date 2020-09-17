82bd6f86087dd22703dfe19b3f5f11374f166f4a
royFloresNyc <royfloresnyc@gmail.com>
2020-09-17 17:34:43 -0400

    Send Data

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your code here
+const submitData = (userName, userEmail) => {
+    const url = 'http://localhost:3000/users'
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    }
+
+    return fetch( url, configObj)
+}
